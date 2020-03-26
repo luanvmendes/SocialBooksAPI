318da3dd4118d13b52007f0268bdb8a386fcf302
luanvmendes <luan_vmendes@hotmail.com>
2020-03-26 15:27:53 -0300

    Aula 2.13 Manipulando erros com @ExceptionHandler e @ControllerAdvice

---
+++ src/main/java/com/gft/socialbooks/services/exceptions/LivroNaoEncontradoException.java
@@ -0,0 +1,20 @@
+package com.gft.socialbooks.services.exceptions;
+
+public class LivroNaoEncontradoException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3456828764562834253L;
+
+	public LivroNaoEncontradoException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public LivroNaoEncontradoException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
